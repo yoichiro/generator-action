489aec8bb5ac4d0163e14146c5c7a3603d37ff7a
Yoichiro Tanaka <yoichiro6642@gmail.com>
2019-03-08 17:03:46 +0900

    Add a support for Google AppEngine for Java.

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,6 +5,14 @@ module.exports = class extends Generator {
   async prompting() {
     this.answers = await this.prompt([
       {
+        type: 'input',
+        name: 'projectDirectoryName',
+        message: 'What is your project directory name?',
+        validate: projectDirectoryName => {
+          return projectDirectoryName !== '';
+        }
+      },
+      {
         type: 'list',
         name: 'actionType',
         message: 'Which type do you want to use for your action?',
@@ -52,12 +60,29 @@ module.exports = class extends Generator {
           name: 'actionName',
           message: 'What is your action name?',
           default: '__TODO:YOUR_ACTION_NAME__'
-        },
+        }
+      ]));
+      if (this.answers.cloudService === 'Firebase Functions' ||
+          this.answers.cloudService === 'Google Cloud Functions') {
+        Object.assign(this.answers, await this.prompt([
+          {
+            type: 'input',
+            name: 'actionProjectId',
+            message: 'What is your action\'s project ID?',
+            default: '__TODO:YOUR_ACTION_PROJECT_ID__'
+          }
+        ]));
+      }
+    }
+    if (this.answers.cloudService === 'Google AppEngine') {
+      Object.assign(this.answers, await this.prompt([
         {
           type: 'input',
-          name: 'actionProjectId',
-          message: 'What is your action\'s project ID?',
-          default: '__TODO:YOUR_ACTION_PROJECT_ID__'
+          name: 'packageName',
+          message: 'What is a full package name for your action\'s classes?',
+          validate: packageName => {
+            return packageName !== '';
+          }
         }
       ]));
     }
@@ -77,6 +102,8 @@ module.exports = class extends Generator {
         } else if (this.answers.language === 'JavaScript') {
           this._writeForActionsSdkGoogleCloudFunctionsJavascript();
         }
+      } else if (this.answers.cloudService === 'Google AppEngine') {
+        this._writeForActionsSdkGoogleAppEngineJava();
       }
     } else if (this.answers.actionType === 'Dialogflow') {
       if (this.answers.cloudService === 'Firebase Functions') {
@@ -91,187 +118,295 @@ module.exports = class extends Generator {
         } else if (this.answers.language === 'JavaScript') {
           this._writeForDialogflowGoogleCloudFunctionsJavascript();
         }
+      } else if (this.answers.cloudService === 'Google AppEngine') {
+        this._writeForDialogflowGoogleAppEngineJava();
       }
     }
   }
 
+  _copyFile(templatePath, destinationPath, options) {
+    const _options = options || {};
+    this.fs.copyTpl(
+      this.templatePath(templatePath),
+      this.destinationPath(`${this.answers.projectDirectoryName}/${destinationPath}`),
+      _options
+    );
+  }
+
   // For Dialogflow
 
+  _writeForDialogflowGoogleAppEngineJava() {
+    this._copyFile(
+      'google-appengine.gradlew.bat',
+      'gradlew.bat'
+    );
+    this._copyFile(
+      'google-appengine.gradlew',
+      'gradlew'
+    );
+    this._copyFile(
+      'google-appengine.gradle-wrapper.properties',
+      'gradle/wrapper/gradle-wrapper.properties'
+    );
+    this._copyFile(
+      'google-appengine.gradle-wrapper.jar',
+      'gradle/wrapper/gradle-wrapper.jar'
+    );
+    this._copyFile(
+      'google-appengine.build.gradle',
+      'build.gradle',
+      {
+        packageName: this.answers.packageName
+      }
+    );
+    const packageName = this.answers.packageName.replace(/\./g, '/');
+    this._copyFile(
+      'google-appengine.fulfillment-servlet.java',
+      `src/main/${packageName}/FulfillmentServlet.java`,
+      {
+        packageName: this.answers.packageName
+      }
+    );
+    this._copyFile(
+      'dialogflow.google-appengine.fulfillment-app.java',
+      `src/main/java/${packageName}/FulfillmentApp.java`,
+      {
+        packageName: this.answers.packageName
+      }
+    );
+    this._copyFile(
+      'google-appengine.appengine-web.xml',
+      'src/main/webapp/WEB-INF/appengine-web.xml'
+    );
+  }
+
   _writeForDialogflowGoogleCloudFunctionsTypescript() {
-    this.fs.copyTpl(
-      this.templatePath('dialogflow.google-cloud-functions.typescript.package.json'),
-      this.destinationPath('package.json')
+    this._copyFile(
+      'dialogflow.google-cloud-functions.typescript.package.json',
+      'package.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('typescript.tsconfig.json'),
-      this.destinationPath('tsconfig.json')
+    this._copyFile(
+      'typescript.tsconfig.json',
+      'tsconfig.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('typescript.tslint.json'),
-      this.destinationPath('tslint.json')
+    this._copyFile(
+      'typescript.tslint.json',
+      'tslint.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('dialogflow.typescript.index.ts'),
-      this.destinationPath('src/index.ts')
+    this._copyFile(
+      'dialogflow.typescript.index.ts',
+      'src/index.ts'
     );
   }
 
   _writeForDialogflowGoogleCloudFunctionsJavascript() {
-    this.fs.copyTpl(
-      this.templatePath('dialogflow.google-cloud-functions.javascript.package.json'),
-      this.destinationPath('package.json')
+    this._copyFile(
+      'dialogflow.google-cloud-functions.javascript.package.json',
+      'package.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('dialogflow.javascript.index.js'),
-      this.destinationPath('index.js')
+    this._copyFile(
+      'dialogflow.javascript.index.js',
+      'index.js'
     );
   }
 
   _writeForDialogflowFirebaseFunctionsJavascript() {
-    this.fs.copyTpl(
-      this.templatePath('firebase-functions.function.json'),
-      this.destinationPath('function.json')
+    this._copyFile(
+      'firebase-functions.function.json',
+      'function.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('dialogflow.firebase-functions.javascript.package.json'),
-      this.destinationPath('functions/package.json')
+    this._copyFile(
+      'dialogflow.firebase-functions.javascript.package.json',
+      'functions/package.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('dialogflow.javascript.index.js'),
-      this.destinationPath('functions/index.js')
+    this._copyFile(
+      'dialogflow.javascript.index.js',
+      'functions/index.js'
     );
   }
 
   _writeForDialogflowFirebaseFunctionsTypescript() {
-    this.fs.copyTpl(
-      this.templatePath('firebase-functions.function.json'),
-      this.destinationPath('function.json')
+    this._copyFile(
+      'firebase-functions.function.json',
+      'function.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('dialogflow.firebase-functions.typescript.package.json'),
-      this.destinationPath('functions/package.json')
+    this._copyFile(
+      'dialogflow.firebase-functions.typescript.package.json',
+      'functions/package.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('typescript.tsconfig.json'),
-      this.destinationPath('functions/tsconfig.json')
+    this._copyFile(
+      'typescript.tsconfig.json',
+      'functions/tsconfig.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('typescript.tslint.json'),
-      this.destinationPath('functions/tslint.json')
+    this._copyFile(
+      'typescript.tslint.json',
+      'functions/tslint.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('typescript.index.ts'),
-      this.destinationPath('functions/src/index.ts')
+    this._copyFile(
+      'typescript.index.ts',
+      'functions/src/index.ts'
     );
   }
 
   // For Actions SDK
 
-  _writeForActionsSdkGoogleCloudFunctionsTypescript() {
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.action.json'),
-      this.destinationPath('action.json'),
+  _writeForActionsSdkGoogleAppEngineJava() {
+    this._copyFile(
+      'actions-sdk.action.json',
+      'action.json',
       {
         actionName: this.answers.actionName
       }
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.google-cloud-functions.typescript.package.json'),
-      this.destinationPath('package.json'),
+    this._copyFile(
+      'google-appengine.gradlew.bat',
+      'gradlew.bat'
+    );
+    this._copyFile(
+      'google-appengine.gradlew',
+      'gradlew'
+    );
+    this._copyFile(
+      'google-appengine.gradle-wrapper.properties',
+      'gradle/wrapper/gradle-wrapper.properties'
+    );
+    this._copyFile(
+      'google-appengine.gradle-wrapper.jar',
+      'gradle/wrapper/gradle-wrapper.jar'
+    );
+    this._copyFile(
+      'google-appengine.build.gradle',
+      'build.gradle',
+      {
+        packageName: this.answers.packageName
+      }
+    );
+    const packageName = this.answers.packageName.replace(/\./g, '/');
+    this._copyFile(
+      'google-appengine.fulfillment-servlet.java',
+      `src/main/${packageName}/FulfillmentServlet.java`,
+      {
+        packageName: this.answers.packageName
+      }
+    );
+    this._copyFile(
+      'actions-sdk.google-appengine.fulfillment-app.java',
+      `src/main/java/${packageName}/FulfillmentApp.java`,
+      {
+        packageName: this.answers.packageName
+      }
+    );
+    this._copyFile(
+      'google-appengine.appengine-web.xml',
+      'src/main/webapp/WEB-INF/appengine-web.xml'
+    );
+  }
+
+  _writeForActionsSdkGoogleCloudFunctionsTypescript() {
+    this._copyFile(
+      'actions-sdk.action.json',
+      'action.json',
+      {
+        actionName: this.answers.actionName
+      }
+    );
+    this._copyFile(
+      'actions-sdk.google-cloud-functions.typescript.package.json',
+      'package.json',
       {
         actionProjectId: this.answers.actionProjectId
       }
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.typescript.tsconfig.json'),
-      this.destinationPath('tsconfig.json')
+    this._copyFile(
+      'actions-sdk.typescript.tsconfig.json',
+      'tsconfig.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.typescript.tslint.json'),
-      this.destinationPath('tslint.json')
+    this._copyFile(
+      'actions-sdk.typescript.tslint.json',
+      'tslint.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.typescript.index.ts'),
-      this.destinationPath('src/index.ts')
+    this._copyFile(
+      'actions-sdk.typescript.index.ts',
+      'src/index.ts'
     );
   }
 
   _writeForActionsSdkGoogleCloudFunctionsJavascript() {
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.action.json'),
-      this.destinationPath('action.json'),
+    this._copyFile(
+      'actions-sdk.action.json',
+      'action.json',
       {
         actionName: this.answers.actionName
       }
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.google-cloud-functions.javascript.package.json'),
-      this.destinationPath('package.json'),
+    this._copyFile(
+      'actions-sdk.google-cloud-functions.javascript.package.json',
+      'package.json',
       {
         actionProjectId: this.answers.actionProjectId
       }
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.javascript.index.js'),
-      this.destinationPath('index.js')
+    this._copyFile(
+      'actions-sdk.javascript.index.js',
+      'index.js'
     );
   }
 
   _writeForActionsSdkFirebaseFunctionsJavascript() {
-    this.fs.copyTpl(
-      this.templatePath('firebase-functions.function.json'),
-      this.destinationPath('function.json')
+    this._copyFile(
+      'firebase-functions.function.json',
+      'function.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.action.json'),
-      this.destinationPath('action.json'),
+    this._copyFile(
+      'actions-sdk.action.json',
+      'action.json',
       {
         actionName: this.answers.actionName
       }
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.firebase-functions.javascript.package.json'),
-      this.destinationPath('functions/package.json'),
+    this._copyFile(
+      'actions-sdk.firebase-functions.javascript.package.json',
+      'functions/package.json',
       {
         actionProjectId: this.answers.actionProjectId
       }
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.javascript.index.js'),
-      this.destinationPath('functions/index.js')
+    this._copyFile(
+      'actions-sdk.javascript.index.js',
+      'functions/index.js'
     );
   }
 
   _writeForActionsSdkFirebaseFunctionsTypescript() {
-    this.fs.copyTpl(
-      this.templatePath('firebase-functions.function.json'),
-      this.destinationPath('function.json')
+    this._copyFile(
+      'firebase-functions.function.json',
+      'function.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.action.json'),
-      this.destinationPath('action.json'),
+    this._copyFile(
+      'actions-sdk.action.json',
+      'action.json',
       {
         actionName: this.answers.actionName
       }
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.firebase-functions.typescript.package.json'),
-      this.destinationPath('functions/package.json'),
+    this._copyFile(
+      'actions-sdk.firebase-functions.typescript.package.json',
+      'functions/package.json',
       {
         actionProjectId: this.answers.actionProjectId
       }
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.typescript.tsconfig.json'),
-      this.destinationPath('functions/tsconfig.json')
+    this._copyFile(
+      'actions-sdk.typescript.tsconfig.json',
+      'functions/tsconfig.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.typescript.tslint.json'),
-      this.destinationPath('functions/tslint.json')
+    this._copyFile(
+      'actions-sdk.typescript.tslint.json',
+      'functions/tslint.json'
     );
-    this.fs.copyTpl(
-      this.templatePath('actions-sdk.typescript.index.ts'),
-      this.destinationPath('functions/src/index.ts')
+    this._copyFile(
+      'actions-sdk.typescript.index.ts',
+      'functions/src/index.ts'
     );
   }
 
