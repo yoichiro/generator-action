7c92b9546152366599784ccfd1abe82dbd2abb73
Yoichiro Tanaka <yoichiro6642@gmail.com>
2019-03-08 18:45:32 +0900

    Execute installing dependencies.

--- generators/app/config.js
+++ generators/app/config.js
@@ -3,6 +3,10 @@ module.exports = {
     'skip-welcome-message': {
       desc: 'Skips the welcome message',
       type: Boolean
+    },
+    'skip-install-message': {
+      desc: 'Skips the message after the installation of dependencies',
+      type: Boolean
     }
   }
 };
